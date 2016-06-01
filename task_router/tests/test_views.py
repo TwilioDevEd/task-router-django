600a914abf0829a974762bc5ae573438848833d4
Marcos Oliveira Sampaio <mosampaio@gmail.com>
2016-06-01 16:01:37 -0300

    Adjust incoming test

--- task_router/tests/test_views.py
+++ task_router/tests/test_views.py
@@ -16,19 +16,14 @@ class HomePageTest(TestCase, XmlTestCase):
         # tests to make sure it works. We'll check for a bit of copy, though
         self.assertIn('Task Router', str(response.content))
 
-    @skip("WIP")
+    # @skip("WIP")
     def test_incoming_call(self):
         # Act
         response = self.client.get('/call/incoming/')
+        content = response.content
+        root = self.assertXmlDocument(content)
 
-        root = self.assertXmlDocument(response.data)
-        say = root.xpath('./Gather/say()')
-# <Response>
-#   <Gather action="/call/enqueue" numDigits="1" timeout="5">
-#     <Say>For ACME Rockets, press one.&</Say>
-#     <Say>For ACME TNT, press two.</Say>
-#   </Gather>
-# </Response>
-        self.assertEquals(2, len(sat), response.data)
-        self.assertEquals('For ACME Rockets, press one.', say[0])
-        self.assertEquals('For ACME TNT, press two.', say[1])
+        say = root.xpath('./Gather/Say/text()')
+
+        self.assertEquals(1, len(say), content)
+        self.assertEquals('For ACME Rockets, press one. For ACME TNT, press two.', say[0])
