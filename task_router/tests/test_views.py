4dca51fe6cd976c0312156ab32f787cecbb765a2
Marcos Oliveira Sampaio <mosampaio@gmail.com>
2016-06-01 15:30:19 -0300

    Add test for test incoming call

--- task_router/tests/test_views.py
+++ task_router/tests/test_views.py
@@ -1,7 +1,8 @@
+from xmlunittest import XmlTestCase
 from django.test import TestCase, Client
+from unittest import skip
 
-
-class HomePageTest(TestCase):
+class HomePageTest(TestCase, XmlTestCase):
 
     def setUp(self):
         self.client = Client()
@@ -14,3 +15,20 @@ class HomePageTest(TestCase):
         # This is a class-based view, so we can mostly rely on Django's own
         # tests to make sure it works. We'll check for a bit of copy, though
         self.assertIn('Task Router', str(response.content))
+
+    @skip("WIP")
+    def test_incoming_call(self):
+        # Act
+        response = self.client.get('/')
+
+        root = self.assertXmlDocument(response.data)
+        say = root.xpath('./Gather/say()')
+# <Response>
+#   <Gather action="/call/enqueue" numDigits="1" timeout="5">
+#     <Say>For ACME Rockets, press one.&</Say>
+#     <Say>For ACME TNT, press two.</Say>
+#   </Gather>
+# </Response>
+        self.assertEquals(2, len(sat), response.data)
+        self.assertEquals('For ACME Rockets, press one.', say[0])
+        self.assertEquals('For ACME TNT, press two.', say[1])
