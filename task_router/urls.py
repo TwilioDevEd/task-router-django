0dd8d3e853667002c0fee971ad2746ac6553d96e
Marcos Oliveira Sampaio <mosampaio@gmail.com>
2016-06-01 16:03:23 -0300

    Add incoming endpoint

--- task_router/urls.py
+++ task_router/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import url
 
 from .views import root
+from .views import incoming_call
 
 urlpatterns = [
     # URLs for searching for and purchasing a new Twilio number
     url(r'^$', root, name='root'),
+    url(r'^call/incoming/?$', incoming_call, name='incoming_call'),
 ]
--- task_router/views.py
+++ task_router/views.py
@@ -1,5 +1,13 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from twilio import twiml
 
 
 def root(request):
     return render(request, 'index.html')
+
+def incoming_call(request):
+    resp = twiml.Response()
+    with resp.gather(numDigits=1, action="/call/enqueue", method="POST") as g:
+        g.say("For ACME Rockets, press one. For ACME TNT, press two.")
+    return HttpResponse(str(resp))
