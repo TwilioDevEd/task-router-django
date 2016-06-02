5ac42168698153e77fb4e6b705a10a0b00a91101
Marcos Oliveira Sampaio <mosampaio@gmail.com>
2016-06-02 14:13:29 -0300

    Revert "adds a template for testing"
    This reverts commit faee2664a62765e3af33302ee118d711ed8f88a6.

--- task_router/views.py
+++ task_router/views.py
@@ -21,15 +21,15 @@ def incoming_call(request):
     resp = twiml.Response()
     with resp.gather(numDigits=1, action="/call/enqueue", method="POST") as g:
         g.say("For ACME Rockets, press one. For ACME TNT, press two.")
-    return HttpResponse(resp, content_type='application/xml')
+    return HttpResponse(resp)
 
 
 @csrf_exempt
 def enqueue(request):
     resp = twiml.Response()
-    with resp.enqueue('abc', workflowSid=WORKFLOW_SID) as g:
+    with resp.enqueue(None, workflowSid=WORKFLOW_SID) as g:
         g.task('{"selected_product": "ACMERockets"}')
-    return HttpResponse(resp, content_type='application/xml')
+    return HttpResponse(resp)
 
 
 @csrf_exempt
