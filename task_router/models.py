5004547c53811253bc165c55bfaaad977024fb21
Victor Shyba <victor.shyba@gmail.com>
2016-06-10 11:02:24 -0300

    Adds phonenumbers formatter
    This library formats phone numbers in a better way, also abstracting the
    old formatter which was way too complex.

--- task_router/models.py
+++ task_router/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+import phonenumbers
+from phonenumbers import PhoneNumberFormat
 
 
 class MissedCall(models.Model):
@@ -7,6 +9,8 @@ class MissedCall(models.Model):
     phone_number = models.CharField(max_length=30)
     created = models.DateTimeField(auto_now_add=True)
 
-    def formatted_phone(self):
-        return format(int(self.phone_number[:-1]), ",").replace(",", "-") + self.phone_number[-1]
-
+    @property
+    def international_phone_number(self):
+        parsed_number = phonenumbers.parse(self.phone_number)
+        return phonenumbers.format_number(parsed_number,
+                                          PhoneNumberFormat.INTERNATIONAL)
