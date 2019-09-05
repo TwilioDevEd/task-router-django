7d2c4ec49a5eeb881b58db8a86e237c926b90441
Samuel Mendes <samuel.mendes@gmail.com>
2019-09-05 15:05:05 +0200

    replace Postgres by SQLite

--- twilio_sample_project/settings/common.py
+++ twilio_sample_project/settings/common.py
@@ -113,8 +113,8 @@ WSGI_APPLICATION = 'twilio_sample_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'task_router'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
